ac089c32304bff0dc3203b798a1c788aa3e1a856
samskinnerphd <samskinnerphd@gmail.com>
2017-05-07 12:12:16 -0400

    clean up controller code

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :line_items
   resources :orders
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
