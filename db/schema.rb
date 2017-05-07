1f221c7741078fcd92dbcc27b6a8098d4dc68028
samskinnerphd <samskinnerphd@gmail.com>
2017-05-07 10:11:01 -0400

    add Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507135836) do
+ActiveRecord::Schema.define(version: 20170507140757) do
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal  "sub_total"
+    t.decimal  "sales_tax"
+    t.decimal  "grand_total"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
