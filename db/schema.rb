9c6a35d63fae7fac56fa8743beb4ab1d1fe4170f
samskinnerphd <samskinnerphd@gmail.com>
2017-05-07 13:03:08 -0400

    add paperclip images to Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507144419) do
+ActiveRecord::Schema.define(version: 20170507163537) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -46,8 +46,12 @@ ActiveRecord::Schema.define(version: 20170507144419) do
     t.text     "description"
     t.integer  "rating"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
