242a424e9a4a97036ec00deda3cd6813039fa2af
samskinnerphd <samskinnerphd@gmail.com>
2017-05-07 14:16:12 -0400

    generate Storefront

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :line_items
   resources :orders
-  resources :products
+  resources :products  
+
+  get 'by_category', to: 'storefront#items_by_category'
+  get 'by_brand',    to: 'storefront#items_by_brand' 
+
+  root 'storefront#all_items'
 end
