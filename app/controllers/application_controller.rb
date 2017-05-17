1db73e640760d38d18bb28ace06cea9eee4a5c84
samskinnerphd <samskinnerphd@gmail.com>
2017-05-17 12:02:17 -0400

    refactor storefront views into partial, activate all views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :load_nav_items
+
+  def load_nav_items
+    @categories = Category.all
+    @brands = Product.pluck(:brand).sort.uniq
+    @cart_count = LineItem.count
+  end
 
   protected
 
