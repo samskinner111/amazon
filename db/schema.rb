65a929724d6f2bbff5eee1379166068d2852d4ad
samskinnerphd <samskinnerphd@gmail.com>
2017-05-17 15:02:39 -0400

    add order_items column to Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515213601) do
+ActiveRecord::Schema.define(version: 20170517185235) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170515213601) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "order_items"
   end
 
   create_table "products", force: :cascade do |t|
