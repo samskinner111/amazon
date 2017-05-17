2d0a4adeb6ec3ad8635de22ce7890a882d356e8d
samskinnerphd <samskinnerphd@gmail.com>
2017-05-17 15:01:52 -0400

    add Cart controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'cart/add_to_cart'
+
+  get 'cart/view_order'
+
+  get 'cart/checkout'
+
   root 'storefront#all_items'
 
   devise_for :users
@@ -9,5 +15,8 @@ Rails.application.routes.draw do
   get 'by_category', to: 'storefront#items_by_category'
   get 'by_brand',    to: 'storefront#items_by_brand' 
 
-  
+  get 'view_order' => 'cart#view_order'
+  get 'checkout' => 'cart#checkout'
+  post 'add_to_cart' => 'cart#add_to_cart'
+
 end
