fea95a442306eef8a3874b8c4ceb516b204880b7
samskinnerphd <samskinnerphd@gmail.com>
2017-05-19 17:33:38 -0400

    update routes for Cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'cart/add_to_cart'
-
-  get 'cart/view_order'
-
-  get 'cart/checkout'
-
   root 'storefront#all_items'
 
   devise_for :users
@@ -14,6 +8,10 @@ Rails.application.routes.draw do
 
   get 'by_category', to: 'storefront#items_by_category'
   get 'by_brand',    to: 'storefront#items_by_brand' 
+
+  get 'cart/add_to_cart'
+  get 'cart/view_order'
+  get 'cart/checkout'
 
   get 'view_order' => 'cart#view_order'
   get 'checkout' => 'cart#checkout'
