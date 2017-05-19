0664d6a8da538760b8eb9c07c81e1f3858af53d8
samskinnerphd <samskinnerphd@gmail.com>
2017-05-19 17:33:08 -0400

    set config/development.rb to use better_errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
+  BetterErrors::Middleware.allow_ip! "10.0.2.2"
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
